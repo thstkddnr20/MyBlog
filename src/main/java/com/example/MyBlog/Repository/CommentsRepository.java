fa7b9ca87eb23c9e9bb3065a3e8bcc9aac66caf6
thstkddnr20 <thstkddnr20@naver.com>
2023-12-30 01:25:18 +0900

    1.1 version

--- src/main/java/com/example/MyBlog/Repository/CommentsRepository.java
+++ src/main/java/com/example/MyBlog/Repository/CommentsRepository.java
@@ -2,7 +2,6 @@ package com.example.MyBlog.Repository;
 
 import com.example.MyBlog.Domain.Articles;
 import com.example.MyBlog.Domain.Comments;
-import com.example.MyBlog.Domain.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
